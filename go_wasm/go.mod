ff7b7e0f0eddd1c38dda60f7eb86f92faafea57e
tyler retzlaff <tyler@speartech.org>
2025-07-17 08:30:20 -0400

    committing test site

---
+++ go_wasm/go.mod
@@ -0,0 +1,3 @@
+module example/go_wasm
+
+go 1.24.0
---
+++ manifest.js
@@ -0,0 +1,19 @@
+
+{
+    "lang": "en-us",
+    "name": "py_to_go",
+    "short_name": "py_to_go",
+    "description": "",
+    "start_url": "/",
+    "background_color": "#2f3d58",
+    "theme_color": "#2f3d58",
+    "orientation": "any",
+    "display": "standalone",
+    "icons": [
+        {
+            "src": "",
+            "type": "image/png",
+            "sizes": "512x512"
+        }
+    ]
+}
