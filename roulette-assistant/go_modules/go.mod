c308b1c5683473d00ce56dca46e78389388b27df
tyler retzlaff <tyler@speartech.org>
2025-09-26 16:54:27 -0400

    updated app without pwa and added roulette-assistant

---
+++ roulette-assistant/go_modules/go.mod
@@ -0,0 +1,3 @@
+module example/go_module
+
+go 1.19
---
+++ roulette-assistant/manifest.js
@@ -0,0 +1,19 @@
+
+{
+    "lang": "en-us",
+    "name": "Roulette_Assist",
+    "short_name": "Roulette_Assist",
+    "description": "",
+    "start_url": "/",
+    "background_color": "#2f3d58",
+    "theme_color": "#2f3d58",
+    "orientation": "any",
+    "display": "standalone",
+    "icons": [
+        {
+            "src": "",
+            "type": "image/png",
+            "sizes": "512x512"
+        }
+    ]
+}
